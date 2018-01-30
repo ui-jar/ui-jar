474b726d6e03d428fa041c874134706c2a3c4264
Kristoffer Nording <krinor@gmail.com>
2018-01-30 23:06:40 +0100

    Updating example with beta 12

--- examples/example-app-with-angular-cli/src/app/ui/progress-bar/progress-bar.component.ts
+++ examples/example-app-with-angular-cli/src/app/ui/progress-bar/progress-bar.component.ts
@@ -16,6 +16,9 @@ export class ProgressBarComponent {
   set value(value: number) {
     this._value = value +'%';
   }
+  get value() {
+    return parseFloat(this._value.split('%')[0]);
+  }
 }
 
 type ProgressBarType = "indefinite" | "definite";
